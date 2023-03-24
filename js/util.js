6c3df935811fcca688c8edefa9748f3fed7f977d
Тимофей <tima_kadochnikov@mail.ru>
2023-03-24 18:25:33 +0500

    upd: проверка на клавишу enter / esc

--- js/util.js
+++ js/util.js
@@ -2,4 +2,8 @@ const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)
 
 const getRandomArray = (array) => array[getRandomNumber(0, array.length - 1)];
 
-export {getRandomNumber, getRandomArray};
+const isEnterKey = (evt) => evt.key === 'Enter';
+
+const isEscapeKey = (evt) => evt.ley === 'Escape';
+
+export {getRandomNumber, getRandomArray, isEnterKey, isEscapeKey};
