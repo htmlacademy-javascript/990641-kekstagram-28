e05a76155d1ec692c5259bf6e66874b91ee1bff3
Тимофей <tima_kadochnikov@mail.ru>
2023-03-26 16:48:38 +0500

    upd: добавление проверки на esc

--- js/util.js
+++ js/util.js
@@ -2,8 +2,6 @@ const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)
 
 const getRandomArray = (array) => array[getRandomNumber(0, array.length - 1)];
 
-const isEnterKey = (evt) => evt.key === 'Enter';
+const isEscapeKey = (evt) => evt.key === 'Escape';
 
-const isEscapeKey = (evt) => evt.ley === 'Escape';
-
-export {getRandomNumber, getRandomArray, isEnterKey, isEscapeKey};
+export {getRandomNumber, getRandomArray, isEscapeKey};
