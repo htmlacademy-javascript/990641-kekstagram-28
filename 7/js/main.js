ae1b9a138e26606e39231b41d09e067cae60a559
Keks <keks@htmlacademy.ru>
2023-03-28 15:18:57 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,3 @@
+import './data.js';
+import './render-thumbnails.js';
+import './big-picture.js';
---
+++ 7/js/render-thumbnails.js
@@ -0,0 +1,33 @@
+import {getDescriptionPhoto} from './data.js';
+import {showBigPicture} from './big-picture.js';
+
+const thumbnailsContainer = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const thumbnailFragment = document.createDocumentFragment();
+
+const renderThumbnails = (photosCollection) => {
+
+  photosCollection.forEach(({url, comments, likes, description}) => {
+    const thumbnailElement = thumbnailTemplate.cloneNode(true);
+
+    thumbnailElement.querySelector('.picture__img').src = url;
+    thumbnailElement.querySelector('.picture__comments').textContent = comments.length;
+    thumbnailElement.querySelector('.picture__likes').textContent = likes;
+
+    thumbnailElement.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      showBigPicture(url, comments, likes, description);
+    });
+
+    thumbnailFragment.appendChild(thumbnailElement);
+  });
+
+  return thumbnailsContainer.appendChild(thumbnailFragment);
+};
+
+renderThumbnails(getDescriptionPhoto());
+
+
