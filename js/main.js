0869efc42935f8c0166474318402c22c46c3831e
Тимофей <tima_kadochnikov@mail.ru>
2023-04-10 20:59:03 +0500

    upd: полировка проекта, и замена фотографии-заглушки

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
 import {renderThumbnails} from './render-thumbnails.js';
-import './big-picture.js';
-import './validation.js';
-import './filters.js';
-import './scale.js';
+import {shuffleArray, debounce} from './util.js';
 import {showError} from './alerts.js';
 import {sendRequest} from './fetch.js';
+import './big-picture.js';
+import './validation.js';
+// import './effects.js';
+// import './scale.js';
+import './load-image.js';
 
+
+const DEFAULT_PREVIEW_LOAD = 25;
+const RANDOM_PREVIEW_LOAD = 10;
+
+const filter = document.querySelector('.img-filters');
 let photos = [];
 
+const removeActiveClass = () => {
+  const activeFilter = document.querySelector('.img-filters__button--active');
+  activeFilter.classList.remove('img-filters__button--active');
+};
+
+const removePhotos = () => {
+  const images = document.querySelectorAll('.picture');
+  if (images) {
+    images.forEach((element) => element.remove());
+  }
+};
+
+const filters = {
+  'filter-default': () => {
+    renderThumbnails(photos.slice(0, DEFAULT_PREVIEW_LOAD));
+  },
+  'filter-random': () => {
+    renderThumbnails(shuffleArray(photos.slice())
+      .slice(0, RANDOM_PREVIEW_LOAD));
+  },
+  'filter-discussed': () => {
+    renderThumbnails(photos.slice()
+      .sort((a, b) => b.comments.length - a.comments.length));
+  },
+};
+
 const onSuccess = (data) => {
+  filter.classList.remove('img-filters--inactive');
   photos = data.slice();
-  renderThumbnails(photos);
+  renderThumbnails(photos.slice(0, DEFAULT_PREVIEW_LOAD));
 };
 
 const onError = () => {
@@ -19,4 +53,13 @@ const onError = () => {
 
 sendRequest(onSuccess, onError, 'GET');
 
-export {photos};
+const onFilterClick = debounce((evt) => {
+  if (evt.target.classList.contains('img-filters__button')) {
+    removeActiveClass();
+    removePhotos();
+    evt.target.classList.add('img-filters__button--active');
+    filters[evt.target.id]();
+  }
+});
+
+filter.addEventListener('click', onFilterClick);
